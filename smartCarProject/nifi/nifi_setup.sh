c0fdedec1d282200468ed91e91f00b65622abca2
Kim KiHo <ghfoddl27@gmail.com>
2024-07-15 19:25:46 +0900

    flume-ending

---
+++ smartCarProject/nifi/nifi_setup.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+wget https://dlcdn.apache.org/nifi/1.27.0/nifi-1.27.0-bin.zip
+wget https://dlcdn.apache.org/nifi/1.27.0/nifi-toolkit-1.27.0-bin.zip
+sudo apt install openjdk-17-jdk
+
